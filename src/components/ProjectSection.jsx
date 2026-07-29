fcecd4aba700c945dd70f24e821593c7d08971d8
Elham-Barzegar <elhambarzegar.hs@gmail.com>
2026-07-29 07:07:05 +0330

    fix main page

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,51 +1,34 @@
-import {useState, useEffect} from 'react';
-import axios from "axios";
 import Data from "./Data.json";
 
-
-
-
 const ProjectSection = () => {
-    const [project, setProject] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-
-            try {
-                const res = await axios.get(Data)
-                // console.table(res.config.url)
-                setProject(res.config.url)
-            }catch (error) {
-                console.log(error)
-            }
-        }
-
-        fetchData(Data)
-    }, []);
+    const project = Data;
 
     return (
         <div className="flex flex-wrap gap-5 justify-center items-center m-10" dir="ltr">
-            {project.map((items, index) => {
-                return (
-                    <div className="relative flex flex-col gap-6 mt-6 text-gray-700 bg-white shadow-md rounded-xl w-96 mb-5 clip-border" key={index}>
-                        <div className="relative  mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl ">
-                            <img src={items.mainImg} alt="" className="w-90 h-55" />
-                        </div>
-                        <div className="p-6 line-clamp-4">
-                            <h5 className="block mb-2 ml-4 text-xl font-semibold leading-snug tracking-normal text-blue-950">
-                                {items.title}
-                            </h5>
-                            <p className="block text-base font-light leading-relaxed text-inherit ml-2">{items.properties}</p>
-                        </div>
-                        {/*<div className="p-6 pt-0">*/}
-                        {/*    <button className="text-center outline-dotted p-3 rounded-lg shadow-md">continue</button>*/}
-                        {/*</div>*/}
-
+            {project.map((items, index) => (
+                <div
+                    key={index}
+                    className="relative flex flex-col gap-6 mt-6 text-gray-700 bg-white shadow-md rounded-xl w-96 mb-5 clip-border"
+                >
+                    <div className="relative mx-4 -mt-6 overflow-hidden rounded-xl">
+                        <img
+                            src={items["mainImg"]}
+                            alt={items.title}
+                            className="w-90 h-55"
+                        />
                     </div>
-                )
-            })}
 
+                    <div className="p-6 line-clamp-4">
+                        <h5 className="mb-2 text-xl font-semibold text-blue-950">
+                            {items.title}
+                        </h5>
 
+                        <p>{items.properties}</p>
+                    </div>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
+
 export default ProjectSection;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,21 +7,21 @@ const Skills = () => {
             <h3 className="text-center text-indigo-100 text-3xl font-bold">My Skills</h3>
             <br/> <br/>
             <div className="grid grid-rows-1 md:grid-cols-2 lg:grid-cols-5 gap-5 ml-10">
-                <img src="../../public/images/React.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Next.svg" alt="Next" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Sass-CC6699.svg" alt="Sass" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/HTML5-E34F26.svg" alt="HTML5" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/TypeScript-3178C6.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/JavaScript-F7DF1E.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Vite-646CFF.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Node.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Three.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/CSS3-1572B6.svg" alt="React" className="rounded-3xl w-30 h-10" />
-               <img src="../../public/images/MongoDB-47A248.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Redux-764ABC.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/GitHub-181717.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Axios-5A29E4.svg" alt="React" className="rounded-3xl w-30 h-10" />
-                <img src="../../public/images/Git-F05032.svg" alt="React" className="rounded-3xl w-30 h-10" /> <br/>
+                <img src="/images/React.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Next.svg" alt="Next" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Sass-CC6699.svg" alt="Sass" className="rounded-3xl w-30 h-10" />
+                <img src="/images/HTML5-E34F26.svg" alt="HTML5" className="rounded-3xl w-30 h-10" />
+                <img src="/images/TypeScript-3178C6.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/JavaScript-F7DF1E.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Vite-646CFF.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Node.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Three.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/CSS3-1572B6.svg" alt="React" className="rounded-3xl w-30 h-10" />
+               <img src="/images/MongoDB-47A248.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Redux-764ABC.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/GitHub-181717.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Axios-5A29E4.svg" alt="React" className="rounded-3xl w-30 h-10" />
+                <img src="/images/Git-F05032.svg" alt="React" className="rounded-3xl w-30 h-10" /> <br/>
 
             </div>
         </div>
